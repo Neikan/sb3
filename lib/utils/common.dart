8f308a244acf29659c912c3066b7d4e766f6d6cf
Neikan <sladkov.e.m@outlook.com>
2022-12-17 17:57:11 +0300

    feat(utils): fix getStatusBarBrightness to common

--- lib/utils/common.dart
+++ lib/utils/common.dart
@@ -2,10 +2,5 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-Brightness getStatusBarBrightness({bool? isDark}) => isDark == true
-    ? Platform.isAndroid
-        ? Brightness.light
-        : Brightness.dark
-    : Platform.isAndroid
-        ? Brightness.dark
-        : Brightness.light;
+Brightness getStatusBarBrightness() =>
+    Platform.isAndroid ? Brightness.dark : Brightness.light;
