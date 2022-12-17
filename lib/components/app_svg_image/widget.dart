d6b3ede5d54b726bdbddb0f4273bc22e12151b03
Neikan <sladkov.e.m@outlook.com>
2022-12-17 15:12:14 +0300

    feat(AppSvgImage): add widget

---
+++ lib/components/app_svg_image/widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class AppSvgImage extends StatelessWidget {
+  final String name;
+  final String? semanticsLabel;
+  final Color? color, bgColor;
+  final double? size, bgSize;
+
+  const AppSvgImage({
+    Key? key,
+    required this.name,
+    this.bgColor,
+    this.color,
+    this.semanticsLabel,
+    this.size,
+    this.bgSize,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget icon = SvgPicture.asset(
+      'assets/images/$name.svg',
+      semanticsLabel: semanticsLabel,
+      height: size,
+      width: size,
+      color: color,
+    );
+
+    if (bgColor != null && bgSize != null) {
+      return Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: bgColor,
+        ),
+        height: bgSize,
+        width: bgSize,
+        child: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: icon,
+        ),
+      );
+    }
+
+    return icon;
+  }
+}
